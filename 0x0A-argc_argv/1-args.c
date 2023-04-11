ee3f00074e67b456fb5378e3b1b220c14c86e2c4
taoufik <taoufik@gmail.com>
2023-04-10 17:05:15 -0700

    C - argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of args
+ * @argv: argument vectot
+ * @argc: rgument count
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
