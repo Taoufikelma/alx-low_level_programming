26edf471651a817346908982a7155483adf2f779
taoufik <taoufik@gmail.com>
2023-05-08 14:29:43 -0700

    C file O/I

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,14 +15,14 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int w, o, i;
 
+	if (filename == NULL)
+		return (-1);
+
 	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i];)
 			i++;
 	}
-
-	if (filename == NULL)
-		return (-1);
 
 	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, i);
