25bf0d930bb0f3a2ed9514d86d25f4282db779dd
taoufik <taoufik@gmail.com>
2023-07-10 11:07:37 -0700

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,18 +13,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	int fd;
-	ssize_t bytes;
+	ssize_t r, w;
 
-	if (!filename || letters)
-		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
-	bytes = read(fd, buf, letters);
-	bytes = write(STDOUT_FILENO, buf, bytes);
+	r = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
 
 	free(buf);
 	close(fd);
-	return (bytes);
+	return (w);
 }
